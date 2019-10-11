644d726aeb5ac6c7b4ad49b2bca3032243c1ea3e
Alex <usatij.alex@gmail.com>
2019-10-11 17:13:02 +0300

    Готовое задание

--- lesson 8/script.js
+++ lesson 8/script.js
@@ -10,3 +10,12 @@ var appData = {
     income:[],
     savings: false
 };
+let exps1, exps2;
+exps1 = prompt('Введите обязательную статью расходов в этом месяце','');
+appData.expenses[exps1] = prompt('Во сколько обойдется?', '');
+
+exps2 = prompt('Введите вторую обязательную статью расходов в этом месяце','');
+appData.expenses[exps2] = prompt('Во сколько обойдется?', '');
+
+alert(`Вам понадобится ${appData.expenses[exps1]/30} рублей в день для ${exps1}\n
+ и ${appData.expenses[exps2]/30} рублей в день для ${exps2}` );
