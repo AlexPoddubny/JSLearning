a102fabbf80b001ba3f5db14c1ae4b7b1e196da9
Alex <usatij.alex@gmail.com>
2019-10-15 17:25:07 +0300

    Задание 2

--- lesson 8/script.js
+++ lesson 8/script.js
@@ -1,21 +1,37 @@
 
 //console.log("started");
-var money = prompt("Сколько вы расходуете за месяц?","");
+var money = +prompt("Сколько вы расходуете за месяц?","");
 var time = prompt("Введите дату в формате YYYY-MM-DD","");
 var appData = {
-    budjet: money,
+    budget: money,
     timeData: time,
     expenses:{},
     optionalExpenses:{},
     income:[],
     savings: false
 };
-let exps1, exps2;
-exps1 = prompt('Введите обязательную статью расходов в этом месяце','');
-appData.expenses[exps1] = prompt('Во сколько обойдется?', '');
 
-exps2 = prompt('Введите вторую обязательную статью расходов в этом месяце','');
-appData.expenses[exps2] = prompt('Во сколько обойдется?', '');
+for (let i = 0; i < 2; i++){
+    let a = prompt('Введите обязательную статью расходов №' + (i + 1) + ' в этом месяце',''), 
+        b = prompt('Во сколько обойдется?', '');
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && 
+        a != '' && b != '' && a.length < 50){
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        i--;
+        console.log("no data");
+    }
+    
+}
+console.log(appData);
+appData.moneyPerDay = appData.budget / 30;
 
-alert(`Вам понадобится ${appData.expenses[exps1]/30} рублей в день для ${exps1}\n
- и ${appData.expenses[exps2]/30} рублей в день для ${exps2}` );
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+if (appData.moneyPerDay < 100){
+    console.log("Minimum budget");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log("Medium budget");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("High budget");
+}
